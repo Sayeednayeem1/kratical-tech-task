a77c977bdda422415dbaf5aeddceb47743eb14f5
Abu Sayeed <sayeed@pop-os.localdomain>
2023-02-01 19:41:47 +0600

    added bg color to icons

--- src/components/BenefitsOfCodeReview/BenefitsOfCodeReview.js
+++ src/components/BenefitsOfCodeReview/BenefitsOfCodeReview.js
@@ -12,25 +12,25 @@ const BenefitsOfCodeReview = () => {
                 <h1 className='text-3xl font-bold text-center'>Benefits Associated With Code Review</h1>
                 <hr className='h-1 w-20 bg-orange-600 mb-4 mt-2 mx-auto' />
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-8 container mx-auto'>
-                <div >
-                    <img className='mx-auto' src={icon01} alt="" />
-                    <h1 className='text-center mt-3'>Cost <br /> Saving</h1>
+            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-8  container mx-auto'>
+                <div className='bg-orange-400 py-6 rounded-2xl'>
+                    <img className='mx-auto bg-white p-6 rounded-full' src={icon01} alt="" />
+                    <h1 className='text-center mt-3 text-white'>Cost <br /> Saving</h1>
                 </div>
                 <div>
-                    <img className='mx-auto' src={icon02} alt="" />
+                    <img className='mx-auto  bg-[#FFEEE9] p-6 rounded-full' src={icon02} alt="" />
                     <h1 className='text-center mt-3'>Improve <br /> Code Quality</h1>
                 </div>
                 <div >
-                    <img className='mx-auto' src={icon03} alt="" />
+                    <img className='mx-auto  bg-[#FFEEE9] p-6 rounded-full' src={icon03} alt="" />
                     <h1 className='text-center mt-3'>Identify and Fix <br /> Vulnerabilities</h1>
                 </div>
                 <div >
-                    <img className='mx-auto' src={icon04} alt="" />
+                    <img className='mx-auto  bg-[#FFEEE9] p-6 rounded-full' src={icon04} alt="" />
                     <h1 className='text-center mt-3'>Maintaining <br /> Consistency</h1>
                 </div>
                 <div >
-                    <img className='mx-auto' src={icon05} alt="" />
+                    <img className='mx-auto  bg-[#FFEEE9] p-6 rounded-full' src={icon05} alt="" />
                     <h1 className='text-center mt-3'>Detection <br /> of Errors</h1>
                 </div>
             </div>
