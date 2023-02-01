da6628e6bf46fa994740277bb902e1b4301b299e
Abu Sayeed <sayeed@pop-os.localdomain>
2023-02-01 13:01:48 +0600

    updated the footer with flags and icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+import { AiOutlineMail } from "react-icons/ai";
 
 const Footer = () => {
     return (
@@ -6,16 +8,23 @@ const Footer = () => {
             <div className='max-w-5xl mx-auto'>
                 <footer className="footer grid grid-cols-1 lg:grid-cols-3 p-10 text-neutral-content">
                     <div>
-                        <span className="footer-title">Services</span>
-                        <a className="link link-hover">Branding</a>
+                        <span className=" font-bold text-lg "> <span class="fi fi-us mr-2"></span>UNITED STATES</span>
+                        <p>400 W Peachtree St LW Atlanta <br /> GA, 30303, USA</p>
                     </div>
                     <div>
-                        <span className="footer-title">Company</span>
-                        <a className="link link-hover">About us</a>
+                        <span className=" font-bold text-lg "> <span class="fi fi-in mr-2"></span> INDIA</span>
+                        <p>B-70, second floor-67.<br />Noida - 201301</p>
                     </div>
                     <div>
-                        <span className="footer-title">Legal</span>
-                        <a className="link link-hover">Terms of use</a>
+                        <div className='flex justify-center items-center font-bold text-lg'>
+                            <AiOutlineMail className='mr-2' />
+                            <h2>For Sales:</h2>
+                        </div>
+                        <p>Email: sales@kratical.com</p>
+                        <div className='flex justify-center items-center'>
+                            <span class="fi fi-in mr-2" />
+                            <p>(+91)9289192210</p>
+                        </div>
                     </div>
                 </footer>
             </div>
